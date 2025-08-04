116b7df0231394b920dd2c7e43386d82c80b1f1c
aizatulin69 <kirilajzatulin@gmail.com>
2025-08-04 03:01:00 +0300

    Update 3dot1.py

--- 3.1.py
+++ 3.1.py
@@ -1,27 +1,24 @@
-from datetime import datetime 
+from datetime import datetime
 
-def get_days_from_today(date_tuple):
-    try: # Пробуємо створити об'єкт дати з кортежу (рік, місяць, день)
-        input_date = datetime(*date_tuple)
+def get_days_from_today(date_str):
+    try:
+        # Створюємо обьект datetime зі строки та перевіряємо корректність дати
+        input_date = datetime.strptime(date_str, "%Y-%m-%d")
     except ValueError:
-        print("Помилка: некоректна дата")
+        print("Ошибка: некорректная дата или формат")
         return None
 
+    # розраховуємо кількість днів між датами
     today = datetime.today()
-
-    if input_date > today: 
-        print("Помилка: дата з майбутнього")
-        return None
-
-    # Обчислюємо різницю між сьогоднішньою датою та введеною
     delta = today - input_date
-
     return delta.days
 
-print(get_days_from_today((2020, 2, 29)))  # Коректна дата
-print(get_days_from_today((2020, 2, 30)))  # Некоректна дата
-print(get_days_from_today((2999, 1, 1)))   # Дата з майбутнього
+# Приклади
+print(get_days_from_today("2020-02-29"))  # Корерктна дата
+print(get_days_from_today("2020-02-30"))  # Некоректна дата
+print(get_days_from_today("2999-01-01"))  # Дата з майбутнього
     
 
         
+
 
